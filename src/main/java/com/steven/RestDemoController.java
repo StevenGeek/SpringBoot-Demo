256d6ac06b1b66147f215b793c0d9c7fc55fe760
steven <stevengeek@163.com>
2017-03-09 16:45:55 +0800

    add sqlsessionfactory

--- src/main/java/com/steven/RestDemoController.java
+++ src/main/java/com/steven/RestDemoController.java
@@ -4,7 +4,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +22,8 @@ import com.steven.orm.model.ArthurDemo2;
 @RestController
 @RequestMapping("demo/")
 public class RestDemoController {
+	@Autowired
+	private Environment env;
 	@Autowired
 	private CustomArthurDemo2Mapper customArthurDemo2Mapper;
 	@Autowired
@@ -37,5 +46,12 @@ public class RestDemoController {
 	@RequestMapping("arthurDemoTest")
 	public ArthurDemo2 selectById(){
 		return arthurDemo2Mapper.selectByPrimaryKey(1);
+	}
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(env.getProperty("mybatis.mapper-locations")));
+		return sqlSessionFactoryBean.getObject();
 	}
 }
